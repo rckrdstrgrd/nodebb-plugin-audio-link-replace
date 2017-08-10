0c05b6b4700fe0c68ed0befce69aa49fb93aefb1
Rickard Östegård <rickard.ostergard@gmail.com>
2017-08-10 13:45:43 +0200

    first commit

---
+++ library.js
@@ -0,0 +1,17 @@
+"use strict";
+var plugin = {
+    regexPattern: /<a href=".*\/uploads\/files\/(\w*-(.*\.mp3|.*\.ogg|.*\.wav)).*>(.*)<\/a>/ig,
+    embed: '<audio src="/uploads/files/$1" controls="controls" preload="none"><a href="/uploads/files/$1">$3</a></audio>'
+};
+
+plugin.parse = function (data, callback) {
+    if (!data || !data.postData || !data.postData.content) {
+        return callback(null, data);
+    }
+    if (data.postData.content.match(plugin.regexPattern)) {
+        data.postData.content = data.postData.content.replace(plugin.regexPattern, plugin.embed);
+    }
+    callback(null,data);
+};
+
+module.exports = plugin;
